a8e3668fd077a31fb22366bec2bab9e8223662d7
ferried <harlancui@outlook.com>
2020-01-05 22:40:12 +0800

    feature(all): first commit

---
+++ auth/src/main/java/com/github/rep3/cloud/auth/Rep3AuthApplication.java
@@ -0,0 +1,12 @@
+package com.github.rep3.cloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rep3AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Rep3AuthApplication.class, args);
+    }
+}
---
+++ auth/src/main/java/com/github/rep3/cloud/auth/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.github.rep3.cloud.auth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello";
+    }
+}
