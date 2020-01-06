6e583867bd924a8f31076ec8b23a1b9c5ccbd7aa
ferried <harlancui@outlook.com>
2020-01-06 17:54:27 +0800

    feature(all): add zuul

--- eureka/src/main/java/com/github/rep3/cloud/eureka/Rep3EurekaApplication.java
+++ eureka/src/main/java/com/github/rep3/cloud/eureka/Rep3EurekaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class Rep3EurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(Rep3EurekaApplication.class, args);
     }
 }
--- zuul/src/main/java/com/github/rep3/cloud/zuul/Rep3ZuulApplication.java
+++ zuul/src/main/java/com/github/rep3/cloud/zuul/Rep3ZuulApplication.java
@@ -1,13 +1,15 @@
-package com.github.rep3.cloud.auth;
+package com.github.rep3.cloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableZuulProxy
 @EnableEurekaClient
-public class Rep3AuthApplication {
+@SpringBootApplication
+public class Rep3ZuulApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Rep3AuthApplication.class, args);
+        SpringApplication.run(Rep3ZuulApplication.class, args);
     }
 }
