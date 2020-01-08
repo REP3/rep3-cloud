82d37999fce883bd0203c3816cd62a0bae5c3521
ferried <harlancui@outlook.com>
2020-01-08 10:07:46 +0800

    fix(all): clear some files

---
+++ hystrix/src/main/java/com/github/rep3/cloud/hystrix/Rep3HystrixApplication.java
@@ -0,0 +1,13 @@
+package com.github.rep3.cloud.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringCloudApplication
+@EnableHystrixDashboard
+public class Rep3HystrixApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Rep3HystrixApplication.class, args);
+    }
+}
