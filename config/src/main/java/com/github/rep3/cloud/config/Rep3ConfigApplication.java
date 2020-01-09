870b022f50e2cc573e5ec7340120d782a3829d22
ferried <harlancui@outlook.com>
2020-01-09 14:12:59 +0800

    feature(config): add springbootstartconfig

---
+++ config/src/main/java/com/github/rep3/cloud/config/Rep3ConfigApplication.java
@@ -0,0 +1,9 @@
+package com.github.rep3.cloud.config;
+
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringCloudApplication
+@EnableConfigServer
+public class Rep3ConfigApplication {
+}
