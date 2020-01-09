c470fe4639b58ef48bbd22395b979166eb274452
ferried <harlancui@outlook.com>
2020-01-09 15:33:27 +0800

    fuck the springConfigServer: i need a config file to config config server?WTF

--- ribbon/src/main/java/com/github/rep3/cloud/ribbon/Rep3RibbonApplication.java
+++ ribbon/src/main/java/com/github/rep3/cloud/ribbon/Rep3RibbonApplication.java
@@ -1,9 +1,7 @@
 package com.github.rep3.cloud.ribbon;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringCloudApplication
